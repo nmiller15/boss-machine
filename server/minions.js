61f04e557e8a835f37b20742d3d10788ec86c29a
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-01 20:26:16 -0400

    add get and post request for /minions/

--- server/minions.js
+++ server/minions.js
@@ -9,7 +9,26 @@ const { createMeeting,
     deleteAllFromDatabase, } = require ('./db');
 
 minionsRouter.get('/', (req, res, next) => {
+    const minionsArray = getAllFromDatabase('minions');
+    res.send(minionsArray);
+})
 
+minionsRouter.post('/', (req, res, next) => {
+    const id = null;
+    const name = req.body.name;
+    const title = req.body.title;
+    const salary = Number(req.body.salary);
+    const weaknesses = req.body.weaknesses;
+
+    const instance = {
+        id,
+        name,
+        title,
+        salary,
+        weaknesses
+    }
+    const newMinion = addToDatabase('minions', instance);
+    res.status(201).send(`Successfully created new minion: ${newMinion.name}`);
 })
 
 
