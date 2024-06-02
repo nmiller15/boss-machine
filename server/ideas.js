62f2eb8056793f317518c7a5c22e5c77c723e6a8
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-02 08:36:13 -0400

    feat(POST /ideas)

--- server/ideas.js
+++ server/ideas.js
@@ -15,4 +15,16 @@ ideasRouter.get('/', (req, res, next) => {
     res.status(200).send(ideas);
 })
 
+ideasRouter.post('/', (req, res, next) => {
+    const newIdea = {
+        description: req.body.description,
+        name: req.body.name,
+        numWeeks: req.body.numWeeks,
+        weeklyRevenue: req.body.weeklyRevenue
+    }
+    const added = addToDatabase('ideas', newIdea);
+    if (!added) return res.status(400).send('Not added.');
+    res.status(201).send(`Successfully added idea: ${added.name}`);
+})
+
 module.exports = ideasRouter;
