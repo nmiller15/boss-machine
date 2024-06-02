264f75807acb8b47e37f0589eca1cbc25e421905
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-02 08:25:33 -0400

    feat(GET /ideas)

--- server/ideas.js
+++ server/ideas.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const ideasRouter = express.Router();
+const { createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase, } = require ('./db');
 
+
+ideasRouter.get('/', (req, res, next) => {
+    const ideas = getAllFromDatabase('ideas');
+    res.status(200).send(ideas);
+})
 
 module.exports = ideasRouter;
