8a6616f226ed6f1bcec8316f25be2b88d1a8f544
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-03 00:20:34 -0400

    feat(POST /meetings)

--- server/meetings.js
+++ server/meetings.js
@@ -11,6 +11,12 @@ const { createMeeting,
 meetingsRouter.get('/', (req, res, next) => {
     const meetings = getAllFromDatabase('meetings');
     res.status(200).send(meetings);
+});
+
+meetingsRouter.post('/', (req, res, next) => {
+    const added = addToDatabase('meetings', req.body);
+    if (!added) return res.status(400).send('Instance is not valid.');
+    res.status(201).send(`Successfully added meeting on ${added.day}`);
 })
 
 module.exports = meetingsRouter;
