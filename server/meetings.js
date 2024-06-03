a415b1bba8916c5b23812bcfa2b36ee8a825d835
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-06-03 00:14:28 -0400

    feat(GET /meetings)

--- server/meetings.js
+++ server/meetings.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const meetingsRouter = express.Router();
+const { createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase, } = require ('./db');
+
+meetingsRouter.get('/', (req, res, next) => {
+    const meetings = getAllFromDatabase('meetings');
+    res.status(200).send(meetings);
+})
 
 module.exports = meetingsRouter;
